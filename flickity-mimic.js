016f79810d69ed067fd8bbe143cd8fec650fab54
Hayden Hunter <haydenhunter@Haydens-MacBook-Air.local>
2016-09-07 17:48:35 +1200

    Baby’s first commit
    Stealing project scaffolding from Flickity’s As Nav For

---
+++ flickity-mimic.js
@@ -0,0 +1,106 @@
+/*!
+ * Flickity mimic v1.0.0
+ * enable mimicry for Flickity
+ */
+
+/*jshint browser: true, undef: true, unused: true, strict: true*/
+
+( function( window, factory ) {
+  // universal module definition
+  /*jshint strict: false */ /*globals define, module, require */
+  if ( typeof define == 'function' && define.amd ) {
+    // AMD
+    define( [
+      'flickity/js/index',
+      'fizzy-ui-utils/utils'
+    ], factory );
+  } else if ( typeof module == 'object' && module.exports ) {
+    // CommonJS
+    module.exports = factory(
+      require('flickity'),
+      require('fizzy-ui-utils')
+    );
+  } else {
+    // browser global
+    window.Flickity = factory(
+      window.Flickity,
+      window.fizzyUIUtils
+    );
+  }
+
+}( window, function factory( Flickity, utils ) {
+
+'use strict';
+
+// -------------------------- mimic prototype -------------------------- //
+
+Flickity.defaults.mimic = {
+  indexOffset: 0
+};
+
+Flickity.createMethods.push('_mimic');
+
+var proto = Flickity.prototype;
+
+proto._mimic = function() {
+  this.on( 'activate', this.activateMimic );
+  this.on( 'deactivate', this.deactivateMimic );
+  this.on( 'destroy', this.destroyMimic );
+
+  var mimicOption = this.options.mimic;
+  if ( !mimicOption ) {
+    return;
+  }
+  // HACK do async, give time for other flickity to be initalized
+  var _this = this;
+  setTimeout( function initNavCompanion() {
+    _this.setNavCompanion( mimicOption );
+  });
+};
+
+proto.setNavCompanion = function( elem ) {
+  elem = utils.getQueryElement( elem );
+  var companion = Flickity.data( elem );
+  // stop if no companion or companion is self
+  if ( !companion || companion == this ) {
+    return;
+  }
+
+  this.navCompanion = companion;
+  // companion select
+  var _this = this;
+  companion.on( 'select', this.onNavCompanionSelect );
+};
+
+proto.navCompanionSelect = function( isInstant ) {
+  if ( !this.navCompanion ) {
+    return;
+  }
+
+  // select slide that matches first cell of slide
+  var selectedCell = this.navCompanion.selectedCells[0];
+
+  this.selectCell( selectIndex, false, isInstant );
+
+  this.options.indexOffset
+};
+
+proto.activateMimic = function() {
+  this.navCompanionSelect( true );
+};
+
+proto.deactivateMimic = function() {
+};
+
+proto.destroyMimic = function() {
+  if ( !this.navCompanion ) {
+    return;
+  }
+  delete this.navCompanion;
+};
+
+// -----  ----- //
+
+return Flickity;
+
+}));
